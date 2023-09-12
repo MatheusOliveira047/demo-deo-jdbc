337af49fa854c871f4328d3280f35bca63e51bad
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-09-12 08:43:04 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,15 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import model.entities.Department;
 
 public class Program {
 
     public static void main(String[] args) {
+
+        Department obj = new Department(1,"Books");
+        System.out.println(obj);
+
         Connection conn = DB.getConnection();
         DB.closeConnection();
 
