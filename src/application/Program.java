f2b1856a21222d8d2489a9c6f2c5447b21b12550
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-09-12 15:36:46 -0300

    findAll implementation and insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -12,9 +13,8 @@ import model.entities.Seller;
 
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
 
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
@@ -24,9 +24,14 @@ public class Program {
         List<Seller> sellers = sellerDao.findByDepartment(new Department(2,"Books"));
         sellers.forEach(System.out::println);
 
+        System.out.println("=== TEST 4: Insert seller ===");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Seller seller1 = new Seller(null,"Vanessa","vanessa@gmail.com",sdf.parse("20/04/1998"),8000.00,new Department(1,"Books"));
+        sellerDao.insert(seller1);
 
-
-
+        System.out.println("=== TEST 3: sellers findAll ===");
+        List<Seller> sellers1 = sellerDao.findAll();
+        sellers1.forEach(System.out::println);
 
     }
 
