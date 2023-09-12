a187860e916b6a3a245858b0161ad4dd0ba09c8f
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-09-12 12:52:12 -0300

    findById implemetation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,25 +2,18 @@ package application;
 
 import java.sql.Connection;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
+
 import model.entities.Seller;
 
 public class Program {
 
-    public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Date date = sdf.parse("18/07/1998");
-        Department department = new Department(1,"Developer");
-
-        Seller seller = new Seller(1,"Ludson","ludson@gmail.com",date,3500.00,department);
-
+    public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
