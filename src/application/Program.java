c7b82dbe828e808fd03f2f4eab908a3035c6eae9
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-09-12 09:00:46 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,23 @@
 package application;
 
 import java.sql.Connection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = sdf.parse("18/07/1998");
+        Department department = new Department(1,"Developer");
 
-        Department obj = new Department(1,"Books");
-        System.out.println(obj);
+        Seller seller = new Seller(1,"Ludson","ludson@gmail.com",date,3500.00,department);
+        System.out.println(seller);
 
         Connection conn = DB.getConnection();
         DB.closeConnection();
