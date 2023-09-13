ee53d8ca1114ebf2a3e113a8113a4a6167d0d4c0
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-09-12 21:12:17 -0300

    department update implamention

--- src/application/Program1.java
+++ src/application/Program1.java
@@ -8,12 +8,16 @@ public class Program1 {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("==== Teste 1 insert ====");
+        //System.out.println("==== Teste 1 insert ====");
         Department department = new Department(null,"Backend");
-        departmentDao.insert(department);
+        //departmentDao.insert(department);
 
-        department = new Department(null,"FrontEnd");
-        departmentDao.insert(department);
+        //department = new Department(null,"FrontEnd");
+        //departmentDao.insert(department);
 
+
+        System.out.println("==== Teste 2 update ====");
+        department = new Department(7,"FullStack");
+        departmentDao.update(department);
     }
 }
